b50db38495b03adc72942051d7391a0d89891e2a
pamesite95-cpu <pame.site.95@gmail.com>
2025-10-25 15:57:35 -0300

    Atualiza link Instagram para o perfil de PamelaPaola

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto px-4">
         <span className="font-semibold text-lg mb-4 md:mb-0">Todos os direitos reservados a Perfeição © {new Date().getFullYear()}</span>
         <div className="flex gap-6">
-          <a href="https://instagram.com" target="_blank" rel="noopener" className="hover:text-pink-300 text-2xl"><FaInstagram /></a>
+          <a href="https://www.instagram.com/pamelapaola_shop?igsh=MWR0OGdzcTJjcW05OQ==" target="_blank" rel="noopener" className="hover:text-pink-300 text-2xl"><FaInstagram /></a>
           <a href="https://www.facebook.com/pamelapaola.vieirabarroso" target="_blank" rel="noopener" className="hover:text-pink-300 text-2xl"><FaFacebook /></a>
           <a href="https://wa.me/5591991795010" target="_blank" rel="noopener" className="hover:text-pink-300 text-2xl"><FaWhatsapp /></a>
         </div>
